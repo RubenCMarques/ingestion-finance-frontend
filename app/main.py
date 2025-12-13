147f3f25ed4918774f939aa3e8b2f696cd26dc48
Ruben  Alexandre Chambel Abreu Marques <20240352@novaims.unl.pt>
2025-12-13 14:20:46 +0000

    up

--- app/main.py
+++ app/main.py
@@ -189,7 +189,8 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"],
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login", location="main")
+
 
 if authentication_status is False:
     st.error("Username/password incorretos.")
